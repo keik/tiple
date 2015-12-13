1af1391ea4afa850864b168328d318627fa23c6f
keik <k4t0.kei@gmail.com>
2015-12-14 02:32:36 +0900

    Structure moreover

--- src/main/java/info/keik/tiple/model/Tag.java
+++ src/main/java/info/keik/tiple/model/Tag.java
@@ -3,5 +3,12 @@ package info.keik.tiple.model;
 public class Tag {
 
 	public String name;
-	
+
+	public Tag() {
+	}
+
+	public Tag(String name) {
+		this.name = name;
+	}
+
 }
--- src/main/java/info/keik/tiple/service/QuestionServiceMock.java
+++ src/main/java/info/keik/tiple/service/QuestionServiceMock.java
@@ -1,8 +1,10 @@
 package info.keik.tiple.service;
 
 import info.keik.tiple.model.Question;
+import info.keik.tiple.model.Tag;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -13,9 +15,27 @@ public class QuestionServiceMock implements QuestionService {
 	public List<Question> questions = new ArrayList<>();
 
 	{
-		questions.add(new Question("q1", "this is q1") {{ id = 0; }});
-		questions.add(new Question("q2", "this is q2") {{ id = 1; }});
-		questions.add(new Question("q3", "this is q3") {{ id = 2; }});
+		questions.add(new Question("q1", "this is q1") {
+			{
+				id = 0;
+				Tag[] ts = { new Tag("javascript"), new Tag("jquery") };
+				tags = Arrays.asList(ts);
+			}
+		});
+		questions.add(new Question("q2", "this is q2") {
+			{
+				id = 1;
+				Tag[] ts = { new Tag("java"), new Tag("maven") };
+				tags = Arrays.asList(ts);
+			}
+		});
+		questions.add(new Question("q3", "this is q3") {
+			{
+				id = 2;
+				Tag[] ts = { new Tag("javascript"), new Tag("node") };
+				tags = Arrays.asList(ts);
+			}
+		});
 	}
 
 	@Override
