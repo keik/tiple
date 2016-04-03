ae4643bc57a00c9a9b806b325a3b77f638923487
keik <k4t0.kei@gmail.com>
2016-04-03 16:34:12 +0900

    Configure method security

--- server/src/main/java/info/keik/tiple/config/SecurityConfig.java
+++ server/src/main/java/info/keik/tiple/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package info.keik.tiple.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -24,8 +26,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	UserDetailsService userDetailsService;
 
 	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web
+				.ignoring()
+				.antMatchers("/**/bundle.js", "/**/bundle.css");
+	}
+
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
+		http
+				.authorizeRequests()
+				// create or update or delete question
+				.antMatchers(HttpMethod.POST, "/q").hasRole("USER")
+				.antMatchers(HttpMethod.PUT, "/q").hasRole("USER")
+				.antMatchers(HttpMethod.DELETE, "/q").hasRole("USER")
+				// create or update or delete answer
+				.antMatchers(HttpMethod.POST, "/q/**/answers/").hasRole("USER")
+				.antMatchers(HttpMethod.PUT, "/q/**/answers/").hasRole("USER")
+				.antMatchers(HttpMethod.DELETE, "/q/**/answers/").hasRole("USER")
+				// create or update or delete vote
+				.antMatchers(HttpMethod.POST, "/q/**/votes").hasRole("USER")
+				.antMatchers(HttpMethod.PUT, "/q/**/votes").hasRole("USER")
+				.antMatchers(HttpMethod.DELETE, "/q/**/votes").hasRole("USER")
 				.anyRequest().permitAll()
 
 				.and()
